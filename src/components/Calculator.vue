cdf6d676ee19a9691f362b75389ed4c92ebeb550
54m1r <s.karaouan@gmail.com>
2021-04-10 01:35:20 +0200

    spelling

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -7,7 +7,7 @@
           SGE Champions League Kalkulator
         </h1>
         <h1 class="text-base font-normal text-gray-900">
-          Klicke in der Spalte 'Offene Spiele' Begegbungen an, um die Änderungen
+          Klicke in der Spalte 'Offene Spiele' Begegnungen an, um die Änderungen
           in der Tabelle zu sehen. Tore werden nicht berücksichtigt.
         </h1>
       </div>
